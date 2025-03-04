de754fdf5f69ce8d0bac54c85b33b39a74dd2a0c
unknown <Kavish9904>
2025-03-04 18:16:56 +0530

    render  frontend

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  basePath: "",
+  images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
 };
 
