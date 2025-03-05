7dbac2f6c6379a532e5d6a2e4988fc15d123aea2
unknown <Kavish9904>
2025-03-05 06:46:17 +0530

    fixed

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
