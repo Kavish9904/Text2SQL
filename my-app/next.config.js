d807eb38b51a126da14dd623c26850e5bdf57b21
unknown <Kavish9904>
2025-03-05 07:33:13 +0530

    fixed

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://text2sql-backend.onrender.com/api/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
