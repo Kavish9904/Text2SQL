d225f53c6a348210af28fbdc1c5abc7b20d82d7c
unknown <Kavish9904>
2025-03-05 07:44:46 +0530

    fixed

--- my-app/server.js
+++ my-app/server.js
@@ -7,13 +7,29 @@ const dev = process.env.NODE_ENV !== "production";
 const hostname = process.env.HOST || "0.0.0.0";
 const port = parseInt(process.env.PORT || "3000", 10);
 
-const app = next({ dev, hostname, port });
+const app = next({
+  dev,
+  hostname,
+  port,
+  dir: __dirname,
+  conf: {
+    distDir: ".next",
+    configFile: path.resolve(__dirname, "./next.config.js"),
+  },
+});
+
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+    try {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("Internal Server Error");
+    }
   }).listen(port, hostname, () => {
     console.log(`> Ready on http://${hostname}:${port}`);
   });
