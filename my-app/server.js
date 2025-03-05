c4fbd1413bbc78b173534f082f28e799748fdc1a
unknown <Kavish9904>
2025-03-05 07:11:06 +0530

    fixed

--- my-app/server.js
+++ my-app/server.js
@@ -1,31 +1,29 @@
 const path = require("path");
 const { createServer } = require("http");
 const { parse } = require("url");
-const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
+// Ensure we're in production mode
+process.env.NODE_ENV = "production";
+
+const nextServer = require("./.next/standalone/server.js");
 const hostname = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT, 10) || 3000;
 
-const app = next({ dev, dir: __dirname });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      await handle(req, res, parsedUrl);
-    } catch (err) {
-      console.error("Error occurred handling", req.url, err);
-      res.statusCode = 500;
-      res.end("Internal Server Error");
-    }
+createServer(async (req, res) => {
+  try {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    const parsedUrl = parse(req.url, true);
+    await nextServer.requestHandler(req, res, parsedUrl);
+  } catch (err) {
+    console.error("Error occurred handling", req.url, err);
+    res.statusCode = 500;
+    res.end("Internal Server Error");
+  }
+})
+  .once("error", (err) => {
+    console.error(err);
+    process.exit(1);
   })
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, hostname, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
-    });
-});
+  .listen(port, hostname, () => {
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
